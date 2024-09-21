43d24f49328fcadb6ed767e0e596ae48649dc5b9
abhishekch275 <120980887+abhishekch275@users.noreply.github.com>
2024-09-22 03:20:32 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> ABHISHE CHATURVEDI</strong>
+                <strong className="main-name"> ABHISHEK CHATURVEDI</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
